b10136cc61ea71c35cb507adb2c8ef5a8d40f193
Xystus535 <owarec22@gmail.com>
2025-01-13 08:01:43 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "233535640851"
+global.ownername = "Xtra Sheet"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '233535640851'  //creator number
+global.ownername = '🐛 Xtra Sheet' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
